7554b2c743a397337eba8deebe389ec3a98a7286
Tank Nigella <tanknige@va-c5.kzn.21-school.ru>
2025-02-24 11:32:59 +0300

    fixed design and shared html on components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+import Header from "./Components/Header";
+import Categories from "./Components/Categories";
+import Sort from "./Components/Sort";
+import PizzaCard from "./Components/PizzaCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories/>
+                        <Sort/>
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        <PizzaCard price={400}/>
+                        <PizzaCard price={450}/>
+                        <PizzaCard price={350}/>
+                        <PizzaCard price={250}/>
+                        <PizzaCard price={150}/>
+                        <PizzaCard price={850}/>
+                        <PizzaCard price={450}/>
+                        <PizzaCard price={250}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
